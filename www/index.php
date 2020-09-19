5f7f596fefa8afbbe2b68eeaa4535d89e9d83056
Wânder de Souza Santos <wander.souza.santos@gmail.com>
2020-09-19 19:38:21 -0300

    conceitos iniciais

---
+++ www/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css" />
+</head>
+
+<body>
+    <?php
+    $start = "Vamos começar";
+    echo "<h1> Olá mundo! {$start}</h1>";
+    echo "<p id='js'> Loading... </p>";
+
+    ?>
+    <script src="assets/scripts.js"></script>
+
+</body>
+
+</html>
